1f26a9b69efc0dee3418a876343dcfe5f52dafd2
Karol Nowacki <karol.nowacki@capgemini.com>
2022-12-16 23:46:48 +0100

    new thymeleaf templates: adding base and ingredient, deleting base by id

--- src/main/java/com/example/myOwn/Controller/UIController.java
+++ src/main/java/com/example/myOwn/Controller/UIController.java
@@ -1,11 +1,13 @@
 package com.example.myOwn.Controller;
 
+import com.example.myOwn.Entities.Base;
+import com.example.myOwn.Entities.Ingredient;
 import com.example.myOwn.Entities.Smoothie;
 import com.example.myOwn.Service.SmoothieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -28,5 +30,46 @@ public class UIController {
         model.addAttribute("smoothieCollection",smoothieCollection);
 
         return "list_of_smoothies";
+    }
+
+    @GetMapping("/ui/base")
+    public String showBaseForm(Model model){
+        Collection<Base> baseCollection = service.findAllBases();
+
+        model.addAttribute("baseCollection",baseCollection);
+        model.addAttribute("base", new Base());
+
+        return "add_base";
+    }
+
+    @PostMapping("/ui/base")
+    public String addBase(@ModelAttribute Base base){
+        service.addBase(base);
+
+        return "redirect:/ui/base";
+    }
+
+    @RequestMapping(value="/ui/deleteBase", method = RequestMethod.DELETE)
+    public String deleteBase(@ModelAttribute Base base){
+        service.deleteBaseById(base.getId());
+
+        return "redirect:/ui/base";
+    }
+
+    @GetMapping("/ui/ingredient")
+    public String showIngredientForm(Model model){
+        Collection<Ingredient> ingredientCollection = service.findAllIngredients();
+
+        model.addAttribute("ingredientCollection",ingredientCollection);
+        model.addAttribute("ingredient", new Ingredient());
+
+        return "add_ingredient";
+    }
+
+    @PostMapping("/ui/ingredient")
+    public String addIngredient(Model model, @ModelAttribute Ingredient ingredient){
+        service.addIngredient(ingredient);
+
+        return "redirect:/ui/ingredient";
     }
 }
